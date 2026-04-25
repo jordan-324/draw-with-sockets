b3ea46aadffa8abfa43ba303d61416608e3792ba
j0rdan-324 <jordan.medina@sjsu.edu>
2026-04-25 15:39:12 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require ('express');
+
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
+console.log("hello world");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket) {
+    console.log('new connection: ' + socket.id);
+
+    socket.on('mouse', function(data) {
+        socket.broadcast.emit('mouse', data);
+        console.log(data);
+    });
+}
+
+
+
